c0dac66680d3822db1ef184d508699707e647075
Devin Pastoor <devin.pastoor@gmail.com>
2015-07-10 15:57:49 -0400

    try other react plotting library

--- app/components/IndConcTime.js
+++ app/components/IndConcTime.js
@@ -3,20 +3,20 @@
  * Created by devin on 7/9/2015.
  */
 import React from 'react';
-import {LineChart} from 'react-d3';
+import {LineChart} from 'react-d3-components';
 class IndConcTime extends React.Component {
 
     render() {
-        var styles = {
-                padding: '25px'
-            };
-                return (
-            <div className="col-md-4 col-offset-2" style={styles}>
+        return (
+            <div className="col-md-4">
                 <LineChart
                     legend={this.props.legend}
                     data={this.props.data}
                     width={this.props.width}
                     height={this.props.height}
+                    margin={{top: 10, bottom: 50, left: 50, right: 10}}
+                    xAxis={{innerTickSize: 6, label: "Time"}}
+                    yAxis={{label: "Concentration"}}
                     />
             </div>
         );
