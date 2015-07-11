b3961e4c6fd915fbdfaa3c8e1cc1bd98152159af
Devin Pastoor <devin.pastoor@gmail.com>
2015-07-10 23:04:30 -0400

    pass in data from props for test nvd3 and relabel to key so this plot has what it needs

--- app/components/TestD3.js
+++ app/components/TestD3.js
@@ -10,38 +10,25 @@
 // May NOT actually even need the call to nv.addGraph?
 import React from 'react';
 import nv from 'nvd3';
+import _ from 'lodash';
 class TestD3 extends React.Component {
-    componentDidMount() {
-        nv.addGraph(loadChart);
+    componentDidMount(){
+        let data = _.map(this.props.data, (d) => {
+            {/* nvd3 uses 'key' rather than 'label' for identification of different ids*/}
+            d.key = d.label;
+            return d;
+        });
+        console.log(data);
+        nv.addGraph(loadChart(data));
 
-        function loadChart() {
+        function loadChart(dat) {
 
             var chart = nv.models.lineChart();
             var fitScreen = false;
             var width = 600;
             var height = 300;
             var zoom = 1;
-            var data = [
-				{
-					values: [{x:0,y:0},{x:0.25,y:8.61},{x:0.5,y:19.44},{x:1,y:34.01},{x:2,y:30.23},{x:3,y:31.3},
-						{x:4,y:24.98},{x:6,y:23.38},{x:8,y:23.51},{x:12,y:14.68},{x:16,y:9.07},{x:24,y:5.3}] ,
-					key: "ID1",
-					color: "#ff7f0e"
-				},
-				{
-					values:[{"x":0,"y":0},{"x":0.25,"y":30.4},{"x":0.5,"y":47.63},{"x":1,"y":64.43},{"x":2,"y":100.18},{"x":3,"y":92.56},{"x":4,"y":65.27},
-						{"x":6,"y":49.26},{"x":8,"y":60.29},{"x":12,"y":44.25},{"x":16,"y":41.85},{"x":24,"y":32.9}] ,
-					key: "ID2",
-					color: "#2ca02c"
-				},
-				{
-					values:[{"x":0,"y":0},{"x":0.25,"y":31.4},{"x":0.5,"y":42.63},{"x":1,"y":69.43},{"x":2,"y":91.18},{"x":3,"y":84.56},{"x":4,"y":66.27},
-						{"x":6,"y":52.26},{"x":8,"y":62.29},{"x":12,"y":41.25},{"x":16,"y":38.85},{"x":24,"y":31.9}] ,
-					key: "ID3",
-					color: "#4ecdc4"
-				}
-			];
-
+            var data = dat;
 
 
             chart.useInteractiveGuideline(true);
